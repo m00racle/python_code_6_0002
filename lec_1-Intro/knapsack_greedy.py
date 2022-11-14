7fcffb865da0ae5375e0b6a0cc41370ba83e3f64
mooracle <yanuar.prakosa@gmail.com>
2022-11-14 23:05:14 +0700

    add knapsack greedy code and test
    This is from the READING not from the class session
    
    I also put test for this code in the test directory

---
+++ lec_1-Intro/knapsack_greedy.py
@@ -0,0 +1,118 @@
+class Item(object) :
+    """  
+    Class of Item
+    init Params: name : str, value : float, weight : float
+
+    """
+    def __init__(self, name, value, weight) -> None:
+        self.name = name
+        self.value = float(value)
+        self.weight = float(weight)
+    
+    # make getter function for the class
+    def getName(self) -> str:
+        return self.name
+    def getValue(self) -> float:
+        return self.value
+    def getWeight(self) -> float:
+        return self.weight
+    
+    # define print string representation (str rep) output __str__:
+    def __str__(self) -> str:
+        
+        return '<' + self.name + ', ' + str(self.value) + ', ' + str(self.weight) + '>'
+
+def value(item) -> float:
+    """ 
+    this is key function for sorter based on Item value 
+    Params: item : Item
+    Returns: float
+    """
+    return item.getValue()
+
+def weightInverse(item) -> float:
+    """  
+    this is key function for sorter based on inverse weight (weight)
+    Params: item : Item
+    Returns: float
+    """
+    return 1.0/item.getWeight()
+
+def density(item) -> float:
+    """  
+    this is key function for sorter on density (value / weight)
+    Params: item : Item 
+    Return: float
+    """
+    return item.getValue() / item.getWeight()
+
+def buildItems():
+    """  
+    function to build a list of Item type objects
+    Params : None
+    Return : list of Item type objects
+    """
+    names = ['clock', 'painting', 'radio', 'vase', 'book', 'computer']
+    values = [175, 90, 20, 50, 10, 200]
+    weights = [10, 9, 4, 2, 1, 20]
+
+    # initiate empty list of Items
+    Items = []
+    # for loop iterate all items to be appended in Items
+    for i in range(len(values)):
+        Items.append(Item(names[i], values[i], weights[i]))
+    return Items
+
+def greedy(items, constraint, keyFunction):
+    """  
+    Given list of items sort it according to key function and take the available
+    Parameters: items : list of objects type Item, constraint : the limit in this case weight, keyFunction : function for sort
+    Returns: tuple of (list of Item type objects, total value: float)
+    """
+    totalValue = 0
+    taken = []
+    totalWeight = 0
+    
+    # make a copy of the items but sorted according to keyFunction:
+    # remember we sorted decending from the biggest to the smallest 
+    # this is why we use inverse weight instead of weight to represent weight as consideration
+    itemsCopy = sorted(items, key=keyFunction, reverse=True)
+    for item in itemsCopy:
+        
+        if (totalWeight + item.getWeight()) <= constraint:
+            taken.append(item)
+            totalWeight += item.getWeight()
+            totalValue += item.getValue()
+
+    return (taken, totalValue)
+
+def printGreedy(items, constraint, keyFunction) -> None:
+    """  
+    Given items and constraint print the Greedy algorithm result based on KeyFunction
+    Params: items : [Item], constraint : float, keyFunction : def (object)
+    Return : None
+    """
+    # fetch the Greedy results
+    (taken, totalValue) = greedy(items, constraint, keyFunction)
+    print("Total Values of items taken: ", totalValue)
+    for item in taken :
+        print("   ", item)
+    
+def runGreedy(maxWeight=20):
+    # preps list of all items
+    items = buildItems()
+
+    print("Use Greedy by Value to fill knapsack in size", maxWeight)
+    printGreedy(items, maxWeight, value)
+
+    print("Use Greedy by weight to fill knapsack in size", maxWeight)
+    printGreedy(items, maxWeight, weightInverse)
+
+    print("Use Greedy by Density to fill knapsack in size", maxWeight)
+    printGreedy(items, maxWeight, density)
+
+
+
+if __name__ == '__main__':
+    runGreedy()
+
---
+++ test/test_knapsack_greedy.py
@@ -0,0 +1,72 @@
+import os
+import sys
+
+test_dir = os.path.dirname(__file__)
+code_dir = os.path.normpath(test_dir + "/../lec_1-intro")
+
+sys.path.append(code_dir)
+
+import unittest
+
+import knapsack_greedy as kg
+
+
+class Test_knapsack_greedy(unittest.TestCase):
+    def setUp(self) -> None:
+        self.bagger = kg.Item('bagger', 10, 5)
+        self.items = kg.buildItems()
+    
+    def test_get_name_from_item(self):
+        self.assertEqual(self.bagger.getName(), 'bagger')
+    
+    def test_get_value_float_from_item(self):
+        """ test if the function returns value float of the item """
+        self.assertTrue(type(self.bagger.getValue()) is float)
+    
+    def test_get_value_from_item(self):
+        self.assertEqual(self.bagger.getValue(), 10.0)
+    
+    def test_get_weight_float_from_item(self):
+        self.assertTrue(type(self.bagger.getWeight()) is float)
+
+    def test_get_weight_from_item(self):
+        self.assertEqual(self.bagger.getWeight(), 5.0)
+
+    def test_value_returns_float_value(self):
+        self.assertTrue(type(kg.value(self.bagger)) is float, "returns type not float!")
+        self.assertEqual(kg.value(self.bagger), 10.0, "returned value is wrong")
+
+    def test_value_return_from_weight_function(self):
+        self.assertTrue(type(kg.weightInverse(self.bagger)) is float, "inverse weight returns wrong type")
+        self.assertEqual(kg.weightInverse(self.bagger), 1/5, "inverse weight value is wrong")
+    
+    def test_value_return_from_density_function(self):
+        self.assertTrue(type(kg.density(self.bagger)) is float, "density type is wrong")
+        self.assertEqual(kg.density(self.bagger), 10/5, "densiti VALUE is wrong")
+
+    def test_build_items(self):
+        """ build all listed items into list of items.... """
+        self.assertEqual(len(self.items), 6, "list SIZE is WRONG")
+        self.assertEqual(str(self.items[2]), "<radio, 20.0, 4.0>", "the list SEQUENCE is WRONG")
+
+    # integration testing
+    def test_greedy_by_value(self):
+        (self.taken, self.totalValue) = kg.greedy(self.items, 20.0, kg.value)
+        self.assertEqual(len(self.taken), 1, "taken LIST SIZE is Wrong")
+        self.assertEqual(str(self.taken[0]), "<computer, 200.0, 20.0>", "the CONTENT of the taken list is WORNG")
+        self.assertEqual(self.totalValue, 200.0, "the total value of the taken is WRONG")
+    
+    def test_greedy_by_inverse_weight(self):
+        (self.taken, self.totalValue) = kg.greedy(self.items, 20, kg.weightInverse)
+        self.assertEqual(len(self.taken), 4, "taken LIST SIZE is Wrong")
+        self.assertEqual(str(self.taken[2]), "<radio, 20.0, 4.0>", "the CONTENT of the taken list is WORNG")
+        self.assertEqual(self.totalValue, 170.0, "the total value of the taken is WRONG")
+
+    def test_greedy_by_density(self):
+        (self.taken, self.totalValue) = kg.greedy(self.items, 20.0, kg.density)
+        self.assertEqual(len(self.taken), 4, "taken LIST SIZE is Wrong")
+        self.assertEqual(str(self.taken[1]), "<clock, 175.0, 10.0>", "the CONTENT of the taken list is WORNG")
+        self.assertEqual(self.totalValue, 255.0, "the total value of the taken is WRONG")
+
+if __name__ == '__main__':
+    unittest.main()
