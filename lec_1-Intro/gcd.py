fe14e0e0287d2ff90a4b8c704be0efc4751bade9
mooracle <yanuar.prakosa@gmail.com>
2022-11-27 18:53:06 +0700

    add code for GCD and braille for stackoverflow

---
+++ lec_1-Intro/gcd.py
@@ -0,0 +1,75 @@
+"""  
+finding HCF
+"""
+
+def hcfLoop(x : int, y : int) -> int:
+    """  
+    finding hinghest common factor using loop
+    returns int
+    """
+    while (x % y) > 0:
+        remainder = x % y
+        x = y
+        y = remainder
+    
+    return y
+
+def hcfRecurs(x : int, y : int) -> int:
+    """  
+    find highest common factor using recursion
+    """
+    if y == 0 :
+        return x
+    else:
+        return hcfRecurs(y, x % y)
+
+def solution(s):
+    i = 0
+    m = ""
+    braille = {
+    " " : "000000",
+    "^" : "000001",
+    "a" :   "100000",
+    "b" :   "110000",
+    "c" :   "100100",
+    "d" :   "100110",
+    "e" :   "100010",
+    "f" :   "110100",
+    "g" :   "110110",
+    "h" :   "110010",
+    "i" :   "010100",
+    "j" :   "010110",
+    "k" :   "101000",
+    "l" :   "111000",
+    "m" :   "101100",
+    "n" :   "101110",
+    "o" :   "101010",
+    "p" :   "111100",
+    "q" :   "111110",
+    "r" :   "111010",
+    "s" :   "011100",
+    "t" :   "011110",
+    "u" :   "101001",
+    "v" :   "111001",
+    "w" :   "010111",
+    "x" :   "101101",
+    "y" :   "101111",
+    "z" :   "101011"
+    }
+
+    
+    for k in s: # change i to k as index in for loop
+        if k.isupper() == True:
+            m += braille["^"]+braille[k.lower()]
+        else:
+            m += braille[k]
+        
+    
+    print(m)
+
+solution("hello")
+
+x = 1220
+y = 516
+print(f"the HCF for {x} and {y} using loop: {hcfLoop(x,y)}")
+print(f"the HCF for {x} and {y} using recursion: {hcfRecurs(x,y)}")
---
+++ test/test_gcd.py
@@ -0,0 +1,25 @@
+import os, sys, unittest
+
+test_dir = os.path.dirname(__file__)
+code_dir = os.path.normpath(test_dir + "/../lec_1-intro")
+
+sys.path.append(code_dir)
+
+import gcd as g
+
+class Test_gcd(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_loo_gcd(self):
+        self.assertEqual(g.hcfLoop(1220, 516), 4)
+
+    def test_loop_gcd_commutative(self):
+        self.assertEqual(g.hcfLoop(516, 1220), 4)
+    
+    def test_resurs_gcd_(self):
+        self.assertEqual(g.hcfRecurs(1220, 516), 4)
+    
+    def test_recurs_gcd_commutative(self):
+        self.assertEqual(g.hcfRecurs(516, 1220), 4)
