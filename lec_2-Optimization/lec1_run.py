925397982a5112d3f4959f9b31fe6482f67c0352
mooracle <yanuar.prakosa@gmail.com>
2022-12-16 17:27:11 +0700

    add greedy by density and runtime meter

--- lec_2-Optimization/lec1_run.py
+++ lec_2-Optimization/lec1_run.py
@@ -1,4 +1,5 @@
 from gen_knapsack import Thing, bruteKnapsack, greedy, buildThings, recursiveKnapsack, dynamicKnapsack
+import timeit
 
 def run():
     """  
@@ -19,8 +20,9 @@ def run():
     constraint = 750
     print(f'using Greedy to find best menu below {constraint} calories')
     print(f'\nusing greedy and values as key function:')
+    start = timeit.default_timer()
     greedyVal = greedy(foods, constraint, lambda x: x.getValue())
-    
+    stop = timeit.default_timer()
     total_value = 0
     total_calories = 0
     for food in greedyVal:
@@ -29,9 +31,12 @@ def run():
         print(f'{food},')
     print(f'total value for greedy by value: {total_value}')
     print(f'total calories of greedy by value: {total_calories}')
+    print(f'runtime: {stop - start}')
 
     print(f'\nusing greedy and calories as key function: ')
+    start = timeit.default_timer()
     greedyCalories = greedy(foods, constraint, Thing.getCost, descending=False)
+    stop = timeit.default_timer()
     total_calories = 0
     total_value = 0
     for food in greedyCalories:
@@ -40,9 +45,12 @@ def run():
         total_value += food.getValue()
     print(f'total value for greedy by calories: {total_value}')
     print(f'total calories of greedy by calories: {total_calories}')
+    print(f'runtime: {stop - start}')
 
     print(f'\nusing Greedy by inverse calories as key function:')
+    start = timeit.default_timer()
     greedyInverse = greedy(foods, constraint, lambda x : (1 / x.getCost()))
+    stop = timeit.default_timer()
     total_calories = 0
     total_value = 0
     for food in greedyInverse:
@@ -51,33 +59,57 @@ def run():
         total_value += food.getValue()
     print(f'total value for greedy by inverse calories: {total_value}')
     print(f'total calories of greedy by inverse calories: {total_calories}')
+    print(f'runtime: {stop - start}')
+
+    print(f'\nusing Greedy by density value/calories as key function:')
+    start = timeit.default_timer()
+    greedyDensity = greedy(foods, constraint, lambda x : (x.getValue() / x.getCost()))
+    stop = timeit.default_timer()
+    total_calories = 0
+    total_value = 0
+    for food in greedyDensity:
+        print(f'{food},')
+        total_calories += food.getCost()
+        total_value += food.getValue()
+    print(f'total value for greedy by inverse calories: {total_value}')
+    print(f'total calories of greedy by inverse calories: {total_calories}')
+    print(f'runtime: {stop - start}')
 
     print(f'\nusing Brute Force to find best menu below {constraint} calories:')
+    start = timeit.default_timer()
     total_value, menuList = bruteKnapsack(foods, constraint, Thing.getValue, lambda x : x.getCost())
+    stop = timeit.default_timer()
     total_calories = 0
     for food in menuList :
         print(f'{food}')
         total_calories += food.getCost()
     print(f'total value for Brute force algorithm: {total_value}')
     print(f'total calories for Brute force algorithm: {total_calories}')
+    print(f'runtime: {stop - start}')
 
     # test real life run using recursive and dynamic knapsack
     print(f'\nusing Recursive to find best menu below {constraint} calories:')
+    start = timeit.default_timer()
     recConsider, racAvail, recTaken, recValue, recMemo = recursiveKnapsack(foods, constraint)
+    stop = timeit.default_timer()
     for food in recTaken:
         print(f'{food}')
     print(f'total value using recursion: {recValue}')
     print(f'total calories: {constraint - racAvail}')
     print(f'calls: {recMemo["calls"]}')
+    print(f'runtime: {stop - start}')
 
     print(f'\nusing Dynamic to find best menu below {constraint} calories:')
+    start = timeit.default_timer()
     dynConsider, dynAvail, dynTaken, dynValue, dynMemo = dynamicKnapsack(foods, constraint)
+    stop = timeit.default_timer()
     for food in dynTaken:
         print(f'{food}')
     print(f'total value using recursion: {dynValue}')
     print(f'total calories: {constraint - dynAvail}')
     print(f'calls: {dynMemo["calls"]}')
     print(f'pulls: {dynMemo["pull"]}')
+    print(f'runtime: {stop - start}')
     
 
 if __name__ == '__main__':
