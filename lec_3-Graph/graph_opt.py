947c7e2533045eec3100b6c48500641076ea6c0b
mooracle <yanuar.prakosa@gmail.com>
2023-01-04 15:22:18 +0700

    pass tests for edge and weighted edge

--- lec_3-Graph/graph_opt.py
+++ lec_3-Graph/graph_opt.py
@@ -7,9 +7,12 @@ class Node(object):
     class to represent Node
     """
     def __init__(self, name) -> None:
+        """  
+        init args : name (str)
+        """
         self.name = name
     
-    def getName(self):
+    def getName(self) -> str:
         return self.name
 
     def __str__(self) -> str:
@@ -17,14 +20,44 @@ class Node(object):
 
 class Edge(object):
     """  
-    class to represent Edge in graph 
+    class to represent Edge in graph
+    once it set the source and destination it can't be modified! 
     """
-
+    def __init__(self, source, destination) -> None:
+        """  
+        args : source (Node), destination (Node)
+        """
+        self.source = source
+        self.destination = destination
+    
+    def getSource(self) -> Node:
+        return self.source
+    
+    def getDestination(self) -> Node:
+        return self.destination
+    
+    def __str__(self) -> str:
+        return self.source.getName() + '->' + self.destination.getName()
 class WeightedEdge(Edge):
     """  
     class represent Weighted Edge in graph
     sub-class of Edge
     """
+    def __init__(self, source, destination, weight=1.0) -> None:
+        """  
+        same as init of Edge but add weight (float) as args
+        """
+        self.weight = weight
+        super().__init__(source, destination)
+    
+    def getWeight(self) -> float:
+        return self.weight
+
+    def __str__(self) -> str:
+        """  
+        override the superclass str
+        """
+        return self.source.getName() + '->(' + str(self.weight) + ')' + self.destination.getName()
 
 class Digraph(object):
     """  
