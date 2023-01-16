15d33278d74070fc96dea49135092892de65bc18
mooracle <yanuar.prakosa@gmail.com>
2023-01-16 16:12:32 +0700

    class Graph pass all tests

--- lec_3-Graph/graph_opt.py
+++ lec_3-Graph/graph_opt.py
@@ -63,6 +63,63 @@ class Digraph(object):
     """  
     class represent directed graph 
     """
+    def __init__(self) -> None:
+        self.nodes = []
+        self.edges = {}
+    
+    def addNode(self, node: Node) -> None:
+        """  
+        Given node : Node 
+        append node to nodes list if has not or
+        Raise Value Error
+        Returns None
+        """
+        if node in self.nodes:
+            raise ValueError('Duplicate node')
+        else:
+            self.nodes.append(node)
+            self.edges[node] = []
+
+    def addEdge(self, edge: Edge) -> None:
+        """  
+        Given edge : Edge
+        append the edge into dict edges wiht source node as key and destination node as value
+        or raise Value Error if node is not in list Nodes
+        Returns None
+        """
+        source = edge.getSource()
+        destination = edge.getDestination()
+        if not(source in self.nodes and destination in self.nodes):
+            raise ValueError('Node not in graph')
+        # if the source and destination are listed in nodes then put them into dict edges
+        self.edges[source].append(destination)
+
+    def hasNode(self, node: Node) -> bool:
+        """  
+        Given node : Node
+        Return TRUE if node is in nodes and FALSE otherwise
+        """
+        return node in self.nodes
+
+    def childrenOf(self, node: Node) -> list:
+        """ 
+        Given node : Node 
+        if node in list nodes return list of node children
+        else raise Value Error Node not in graph
+        """
+        if self.hasNode(node):
+            return self.edges[node]
+        else:
+            raise ValueError("Node not in graph")
+
+    def __str__(self) -> str:
+        printed = ''
+        for src in self.nodes:
+            for dest in self.edges[src]:
+                printed = printed + src.getName() + '->' + dest.getName() + '\n'
+        
+        # remember to omit the \n in the last entry
+        return printed[:-1] 
 
 class Graph(Digraph):
     """  
