241e000797f1fc3c755590b34ebf68a54c62cce6
mooracle <yanuar.prakosa@gmail.com>
2023-02-03 20:50:52 +0700

    main Lec_3 add lecture slide example cases

--- lec_3-Graph/main.py
+++ lec_3-Graph/main.py
@@ -26,8 +26,66 @@ def runDFS()->None:
     result = go.DFS(d, n[0], n[5], verbose=True)
     print(f'Shortest path found by DFS: {go.printPath(result)}')
 
+def buildCityGraph(graphType):
+    # creating digraph map
+    g = graphType()
+    # create cities as nodes
+    cities = []
+    boston = Node('Boston')
+    cities.append(boston)
+    providence = Node('Providence')
+    cities.append(providence)
+    new_york = Node('New York')
+    cities.append(new_york)
+    chicago = Node('Chicago')
+    cities.append(chicago)
+    denver = Node('Denver')
+    cities.append(denver)
+    phoenix = Node('Phoenix')
+    cities.append(phoenix)
+    los_angeles = Node('Los Angeles')
+    cities.append(los_angeles)
+
+    for city in cities:
+        g.addNode(city)
+    
+    # input all edges in graph:
+    g.addEdge(Edge(boston, providence))
+    g.addEdge(Edge(boston, new_york))
+    g.addEdge(Edge(providence, boston))
+    g.addEdge(Edge(providence, new_york))
+    g.addEdge(Edge(new_york, chicago))
+    g.addEdge(Edge(chicago, denver))
+    g.addEdge(Edge(chicago, phoenix))
+    g.addEdge(Edge(denver, phoenix))
+    g.addEdge(Edge(denver, new_york))
+    g.addEdge(Edge(los_angeles, boston))
+
+    return g
+
+def testSP(source: str, destination: str):
+    g = buildCityGraph(Digraph)
+    print(f'\nSTART finding shortest path from {source} to {destination}')
+    print(f'using DFS mehtod:')
+    resultDfs = go.DFS(g, g.getNode(source), g.getNode(destination), verbose=True)
+    if resultDfs != None:
+        print(f'Shortes Path by DFS method: {go.printPath(resultDfs)}')
+    else:
+        print(f'DFS method failed to find path from {source} to {destination}')
+
+    print(f'\nusing BFS method:')
+    resultBFS = go.BFS(g,g.getNode(source), g.getNode(destination), verbose=True)
+    if resultBFS != None:
+        print(f'shortest Path by BFS method: {go.printPath(resultBFS)}')
+    else:
+        print(f'BFS method failed to find path from {source} to {destination}')
+
+    
+
 def main():
-    runDFS()
+    testSP('Chicago', 'Boston')
+    # this should not find any path!
+    testSP('Boston', 'Phoenix')
 
 if __name__ == "__main__":
     main()
