7d11e0bbefecf510c7d6e0019d242672d92aa4c0
mooracle <yanuar.prakosa@gmail.com>
2024-03-13 22:03:51 +0700

    make sure lec7.py runs

--- lec_7_confidence_interval/lec7.py
+++ lec_7_confidence_interval/lec7.py
@@ -60,38 +60,19 @@ def getMeanAndStd(X):
     std = (tot/len(X))**0.5
     return mean, std
 
-#random.seed(1)
-#dist, numSamples = [], 1000000
-#
-#for i in range(numSamples):
+# random.seed(1)
+# dist, numSamples = [], 1000000
+# 
+# for i in range(numSamples):
 #    dist.append(random.gauss(0, 100))
 #    
-#weights = [1/numSamples]*len(dist)
-#v = pylab.hist(dist, bins = 100,
+# weights = [1/numSamples]*len(dist)
+# v = pylab.hist(dist, bins = 100,
 #               weights = [1/numSamples]*len(dist))
-#               
-#print('Fraction within ~200 of mean =',
+# 
+# print('Fraction within ~200 of mean =',
 #      sum(v[0][30:70]))
 #
-#def gaussian(x, mu, sigma):
-#  factor1 = (1.0/(sigma*((2*pylab.pi)**0.5)))
-#  factor2 = pylab.e**-(((x-mu)**2)/(2*sigma**2))
-#  return factor1*factor2
-#  
-#xVals, yVals = [], []
-#mu, sigma = 0, 1
-#x = -4
-#while x <= 4:
-#    xVals.append(x)
-#    yVals.append(gaussian(x, mu, sigma))
-#    x += 0.05
-#pylab.plot(xVals, yVals)
-#pylab.title('Normal Distribution, mu = ' + str(mu)\
-#            + ', sigma = ' + str(sigma))
-#
-
-import scipy.integrate
-
 def gaussian(x, mu, sigma):
     """  
     gaussian integral method
@@ -100,23 +81,41 @@ def gaussian(x, mu, sigma):
     factor2 = pylab.e**-(((x-mu)**2)/(2*sigma**2))
     return factor1*factor2
 
-def checkEmpirical(numTrials):
-  for t in range(numTrials):
-     mu = random.randint(-10, 10)
-     sigma = random.randint(1, 10)
-     print('For mu =', mu, 'and sigma =', sigma)
-     for numStd in (1, 1.96, 3):
-        area = scipy.integrate.quad(gaussian,
-                                    mu-numStd*sigma,
-                                    mu+numStd*sigma,
-                                    (mu, sigma))[0]
-        print(' Fraction within', numStd,
-              'std =', round(area, 4))
-        
-#checkEmpirical(3)
 
-##Test CLT
-#def plotMeans(numDice, numRolls, numBins, legend, color, style):
+# xVals, yVals = [], []
+# mu, sigma = 0, 1
+# x = -4
+# while x <= 4:
+#    xVals.append(x)
+#    yVals.append(gaussian(x, mu, sigma))
+#    x += 0.05
+# pylab.plot(xVals, yVals)
+# pylab.title('Normal Distribution, mu = ' + str(mu)\
+#            + ', sigma = ' + str(sigma))
+# pylab.show()
+
+
+import scipy.integrate
+
+
+## Test Check empirical (uncomment)
+# def checkEmpirical(numTrials):
+#   for t in range(numTrials):
+#      mu = random.randint(-10, 10)
+#      sigma = random.randint(1, 10)
+#      print('For mu =', mu, 'and sigma =', sigma)
+#      for numStd in (1, 1.96, 3):
+#         area = scipy.integrate.quad(gaussian,
+#                                     mu-numStd*sigma,
+#                                     mu+numStd*sigma,
+#                                     (mu, sigma))[0]
+#         print(' Fraction within', numStd,
+#               'std =', round(area, 4))
+        
+# checkEmpirical(3)
+
+# #Test CLT
+# def plotMeans(numDice, numRolls, numBins, legend, color, style):
 #    means = []
 #    for i in range(numRolls//numDice):
 #        vals = 0
@@ -127,32 +126,39 @@ def checkEmpirical(numTrials):
 #               weights = [1/len(means)]*len(means),
 #               hatch = style)
 #    return getMeanAndStd(means)
-# 
-#mean, std = plotMeans(1, 1000000, 19, '1 die', 'b', '*')
-#print('Mean of rolling 1 die =', str(mean) + ',', 'Std =', std)
-#mean, std = plotMeans(50, 1000000, 19, 'Mean of 50 dice', 'r', '//')
-#print('Mean of rolling 50 dice =', str(mean) + ',', 'Std =', std)
-#pylab.title('Rolling Continuous Dice')
-#pylab.xlabel('Value')
-#pylab.ylabel('Probability')
-#pylab.legend()
 
-##Test CLT
-#numTrials = 1000000
-#numSpins = 200
-#game = FairRoulette()
-#
-#means = []
-#for i in range(numTrials):
+# mean, std = plotMeans(1, 1000000, 19, '1 die', 'b', '*')
+# print('Mean of rolling 1 die =', str(mean) + ',', 'Std =', std)
+# mean, std = plotMeans(50, 1000000, 19, 'Mean of 50 dice', 'r', '//')
+# print('Mean of rolling 50 dice =', str(mean) + ',', 'Std =', std)
+# pylab.title('Rolling Continuous Dice')
+# pylab.xlabel('Value')
+# pylab.ylabel('Probability')
+# pylab.legend()
+# pylab.show()
+
+# #Test CLT
+# numTrials = 100000
+# # NOTE: I change the number of trials to 100.000 since 1000.000 takes too long to complete.
+# numSpins = 200
+# game = FairRoulette()
+
+# means = []
+# for i in range(numTrials):
 #    means.append(findPocketReturn(game, 1, numSpins,
 #                                  False)[0])
-#
-#pylab.hist(means, bins = 19,
-#           weights = [1/len(means)]*len(means))
-#pylab.xlabel('Mean Return')
-#pylab.ylabel('Probability')
-#pylab.title('Expected Return Betting a Pocket 200 Times')
 
+# pylab.hist(means, bins = 19,
+#           weights = [1/len(means)]*len(means))
+# pylab.xlabel('Mean Return')
+# pylab.ylabel('Probability')
+# pylab.title('Expected Return Betting a Pocket 200 Times')
+# pylab.show()
+
+
+"""  
+pi needle experiment
+"""
 def throwNeedles(numNeedles):
     inCircle = 0
     for Needles in range(1, numNeedles + 1, 1):
@@ -183,5 +189,5 @@ def estPi(precision, numTrials):
         numNeedles *= 2
     return curEst
 
-random.seed(0)
-estPi(0.005, 100)
+# random.seed(0)
+# estPi(0.005, 100)
