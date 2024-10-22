d879f33c8979c49862645d36e19fb666206f2ace
m00racle <33504187+m00racle@users.noreply.github.com>
2024-10-22 09:17:15 +0700

    fix Titanic data file address to absolute by os

--- lec_13_classification/lecture13.py
+++ lec_13_classification/lecture13.py
@@ -4,7 +4,7 @@ Created on Mon Sep 19 11:45:20 2016
 
 @author: johnguttag
 """
-import pylab, random
+import pylab, random, os
 
 #set line width
 pylab.rcParams['lines.linewidth'] = 4
@@ -55,18 +55,18 @@ class Animal(object):
     def __str__(self):
         return self.name
                              
-##Actual number of legs
-#cobra = Animal('cobra', [1,1,1,1,0])
-#rattlesnake = Animal('rattlesnake', [1,1,1,1,0])
-#boa = Animal('boa\nconstrictor', [0,1,0,1,0])
-#chicken = Animal('chicken', [1,1,0,1,2])
-#alligator = Animal('alligator', [1,1,0,1,4])
-#dartFrog = Animal('dart frog', [1,0,1,0,4])
-#zebra = Animal('zebra', [0,0,0,0,4])
-#python = Animal('python', [1,1,0,1,0])
-#guppy = Animal('guppy', [0,1,0,0,0])
-#animals = [cobra, rattlesnake, boa, chicken, guppy,
-#           dartFrog, zebra, python, alligator]
+#Actual number of legs
+cobra = Animal('cobra', [1,1,1,1,0])
+rattlesnake = Animal('rattlesnake', [1,1,1,1,0])
+boa = Animal('boa\nconstrictor', [0,1,0,1,0])
+chicken = Animal('chicken', [1,1,0,1,2])
+alligator = Animal('alligator', [1,1,0,1,4])
+dartFrog = Animal('dart frog', [1,0,1,0,4])
+zebra = Animal('zebra', [0,0,0,0,4])
+python = Animal('python', [1,1,0,1,0])
+guppy = Animal('guppy', [0,1,0,0,0])
+animals = [cobra, rattlesnake, boa, chicken, guppy,
+          dartFrog, zebra, python, alligator]
 #
 ##Binary features only           
 #cobra = Animal('cobra', [1,1,1,1,0])
@@ -114,8 +114,8 @@ def compareAnimals(animals, precision):
     pylab.axis('off')
     pylab.savefig('distances')
 
-#compareAnimals(animals, 3)
-#assert False
+compareAnimals(animals, 3)
+# assert False
 
 class Passenger(object):
     featureNames = ('C1', 'C2', 'C3', 'age', 'male gender')
@@ -144,7 +144,7 @@ def getTitanicData(fname):
     data = {}
     data['class'], data['survived'], data['age'] = [], [], []
     data['gender'], data['name'] = [], []
-    f = open(fname)
+    f = open(os.path.normpath(os.path.dirname(__file__)) + '/' +fname)
     line = f.readline()
     while line != '':
         split = line.split(',')
