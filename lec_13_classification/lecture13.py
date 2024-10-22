a0ef5f10ea8c318c1ef90f016fcdba478328a06d
m00racle <33504187+m00racle@users.noreply.github.com>
2024-10-22 09:23:15 +0700

    commented out all animals to faster init runs

--- lec_13_classification/lecture13.py
+++ lec_13_classification/lecture13.py
@@ -55,18 +55,18 @@ class Animal(object):
     def __str__(self):
         return self.name
                              
-#Actual number of legs
-cobra = Animal('cobra', [1,1,1,1,0])
-rattlesnake = Animal('rattlesnake', [1,1,1,1,0])
-boa = Animal('boa\nconstrictor', [0,1,0,1,0])
-chicken = Animal('chicken', [1,1,0,1,2])
-alligator = Animal('alligator', [1,1,0,1,4])
-dartFrog = Animal('dart frog', [1,0,1,0,4])
-zebra = Animal('zebra', [0,0,0,0,4])
-python = Animal('python', [1,1,0,1,0])
-guppy = Animal('guppy', [0,1,0,0,0])
-animals = [cobra, rattlesnake, boa, chicken, guppy,
-          dartFrog, zebra, python, alligator]
+# #Actual number of legs
+# cobra = Animal('cobra', [1,1,1,1,0])
+# rattlesnake = Animal('rattlesnake', [1,1,1,1,0])
+# boa = Animal('boa\nconstrictor', [0,1,0,1,0])
+# chicken = Animal('chicken', [1,1,0,1,2])
+# alligator = Animal('alligator', [1,1,0,1,4])
+# dartFrog = Animal('dart frog', [1,0,1,0,4])
+# zebra = Animal('zebra', [0,0,0,0,4])
+# python = Animal('python', [1,1,0,1,0])
+# guppy = Animal('guppy', [0,1,0,0,0])
+# animals = [cobra, rattlesnake, boa, chicken, guppy,
+#           dartFrog, zebra, python, alligator]
 #
 ##Binary features only           
 #cobra = Animal('cobra', [1,1,1,1,0])
