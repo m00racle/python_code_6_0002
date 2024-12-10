63f81fd4b8102f2ec72075f4e435bb002cbf2ed9
mooracle <yanuar.prakosa@gmail.com>
2024-12-10 13:41:29 +0700

    fix the pylab plot show in 1st part lec_15
    test and run the second part of random case
    
    test run back the first part and add comment

--- lec_15_closure/lec_15.py
+++ lec_15_closure/lec_15.py
@@ -26,13 +26,15 @@ pylab.rcParams['lines.markersize'] = 10
 #set number of examples shown in legends
 pylab.rcParams['legend.numpoints'] = 1
 
-#temps = [98.6, 99, 99.5, 99.3, 100, 99.9, 100.5]
-#times = range(3,10)
-#pylab.plot(times, temps)
-#pylab.xlabel('Time')
-#pylab.ylabel('Oral Temperature')
-#pylab.title('A Bout with the Flu')
-#pylab.ylim(97,102)
+## Uncomment this part to plot the temps vs times 
+# temps = [98.6, 99, 99.5, 99.3, 100, 99.9, 100.5]
+# times = range(3,10)
+# pylab.plot(times, temps)
+# pylab.xlabel('Time')
+# pylab.ylabel('Oral Temperature')
+# pylab.title('A Bout with the Flu')
+# pylab.ylim(97,102)
+# pylab.show()
 
 random.seed(0)
 numCasesPerYear = 36000
@@ -53,16 +55,17 @@ prob = round(numGreater/numTrials, 4)
 print('Est. probability of region 111 having\
  at least 143 cases =', prob)
 
+## Uncomment these second part of the code and Comment out the first part above to compare:
 
-#numTrials = 100
-#anyRegion = 0
-#for trial in range(numTrials):
+# numTrials = 100
+# anyRegion = 0
+# for trial in range(numTrials):
 #    locs = [0]*numCommunities
 #    for i in range(numYears*numCasesPerYear):
 #        locs[random.choice(range(numCommunities))] += 1
 #    if max(locs) >= 143:
 #        anyRegion += 1
-#print(anyRegion)
-#aProb = round(anyRegion/numTrials, 4)
-#print('Est. probability of some region  having\
+# print(anyRegion)
+# aProb = round(anyRegion/numTrials, 4)
+# print('Est. probability of some region  having\
 # at least 143 cases =', aProb)
